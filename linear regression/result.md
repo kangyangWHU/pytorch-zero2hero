27fe597759977c294d14a7396050fb50150c979c
= <=>
2019-06-26 17:41:44 +0800

    add result

---
+++ linear regression/result.md
@@ -0,0 +1,5 @@
+
+![img]https://github.com/kangyangWHU/pytorch-zero2hero/raw/master/linear regression/linear_regression_biger_lr.gif
+# 上图是learning rate设置的偏大0.003，就会在local minima附近摆动，有可能不会找到local minima
+![img]https://github.com/kangyangWHU/pytorch-zero2hero/raw/master/linear regression/linear_regression_proper_lr.gif
+# 上图的learning rate设置的比较小(1e-5)，故直接找到了local minima
--- linear regression/simple_linear_regression.py
+++ linear regression/simple_linear_regression.py
@@ -23,8 +23,9 @@ def step_gradient(b_current, w_current, points, learning_rate):
     for i in range(len(points)):
         x = points[i,0]
         y = points[i,1]
-        b_gradient = (2/N)*((w_current*x+b_current)-y)
-        w_gradient = (2/N)*((w_current*x+b_current)-y)*x
+        b_gradient += (2/N)*((w_current*x+b_current)-y)
+        w_gradient += (2/N)*((w_current*x+b_current)-y)*x
+       
 
     new_b = b_current - learning_rate*b_gradient        # update the b
     new_w = w_current - learning_rate*w_gradient        # update w
@@ -38,10 +39,9 @@ def gradient_descent(points, init_b, init_w, learning_rate, num_iters):
     '''
     b = init_b
     w = init_w
-
     plt.ion() # 开启interactive mode 成功的关键函数
     plt.figure()
-
+   
     for i in range(num_iters):
 
         b,w = step_gradient(b, w, points, learning_rate)
@@ -50,13 +50,17 @@ def gradient_descent(points, init_b, init_w, learning_rate, num_iters):
         # plot the figure dynamicly
         plt.clf()               # clear the figure
         plt.axis([0,100,0,140])
+        plt.xlabel('learning_rate = 1e-5')
         plt.scatter(points[:,0], points[:,1])
         x = np.arange(0,100,2)
         y = w*x+b
         plt.plot(x,y)
+
         plt.draw()              # after clear the figure, you must call the draw function
         plt.show()
-        plt.pause(0.001)
+        if i == 0:
+            plt.pause(5)
+        plt.pause(0.1)
 
     return [b,w]
 
@@ -65,10 +69,10 @@ def run():
     points = np.genfromtxt('data.csv', delimiter=',')
 
 
-    learning_rate = 1e-4
+    learning_rate = 1e-5
     init_b = 0
     init_w = 0
-    num_iters = 1000
+    num_iters = 100
 
     gradient_descent(points, init_b, init_w, learning_rate, num_iters)
 
